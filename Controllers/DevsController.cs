b9c8a5671ca168e7660eb8d33e99808a06f4c9d4
Raul Juliao <raulj@uninorte.edu.co>
2023-03-02 16:36:03 -0500

    post method modificated to fit requirements and devmodel inner class

--- Controllers/DevsController.cs
+++ Controllers/DevsController.cs
@@ -18,7 +18,7 @@ public class DevsController : ControllerBase
     public async Task<List<Dev>> Get()=>
         await _developerService.GetDevsAsync();
     
-    [HttpGet("{id : lenght(24)}")]
+    [HttpGet("GetById/{id:length(24)}")]
     public async Task<ActionResult<Dev>> GetById(string id){
         var dev = await _developerService.GetDevAsync(id);
 
@@ -31,13 +31,59 @@ public class DevsController : ControllerBase
 
 
     [HttpPost]
-    public async Task<IActionResult> Post(Dev newDev){
+    public async Task<IActionResult> Post(DevModel newDevM){
+        var newDev = new Dev();
+
+        if (newDevM.F_name.Length >= 3 && newDevM.F_name.Length < 20 )
+        {
+            newDev.FirstName = newDevM.F_name;
+        }else 
+        { 
+            return BadRequest("The name is too short or too long"); 
+        }
+        
+        if (newDevM.L_name.Length >= 3 && newDevM.L_name.Length < 30 )
+        {
+            newDev.LastName = newDevM.L_name;
+        }else 
+        { 
+            return BadRequest("The last name is too short or too long"); 
+        }
+        newDev.FullName = newDevM.F_name + " " + newDevM.L_name;
+
+        if (newDevM.Age > 10)
+        {
+            newDev.Age = newDevM.Age;
+        }else 
+        {
+            return BadRequest("We only accept peope with age greater than 10");
+        }
+
+        if (newDevM.WorkedHours > 30 && newDevM.WorkedHours < 50)
+        {
+            newDev.WorkedHours = newDevM.WorkedHours;
+        }else
+        {
+            return BadRequest("The worked hours needs to be between 30 and 50 hours");
+        }
+
+        if (newDevM.SalaryByHours > 13)
+        {
+            newDev.SalaryByHours = newDevM.SalaryByHours;
+        }else 
+        {
+            return BadRequest("The salary needs to be greater than 13");
+        }
+
+        newDev.DeveloperType = newDevM.Dev_type;
+        newDev.email = newDevM.email; 
+
         await _developerService.CreateAsync(newDev);
 
         return CreatedAtAction(nameof(Get), new {id = newDev.Id}, newDev);
     }
 
-    [HttpPut("id : lenght(24)")]
+    [HttpPut("{id:length(24)}")]
     public async Task<IActionResult> Put(string id, Dev updateInfo){
 
         var dev = await _developerService.GetDevAsync(id);
@@ -52,7 +98,7 @@ public class DevsController : ControllerBase
 
     }
 
-    [HttpDelete("id: lenght(24)")]
+    [HttpDelete("{id:length(24)}")]
     public async Task<IActionResult> Delete(string id){
         var dev = await _developerService.GetDevAsync(id);
 
@@ -64,5 +110,15 @@ public class DevsController : ControllerBase
         await _developerService.RemoveOneAsync(id);
 
         return NoContent(); 
+    }
+
+    public class DevModel{
+        public string F_name {get; set;} = null!;
+        public string L_name {get;set;} = null!;
+        public string Dev_type {get; set;} = null!;
+        public int Age {get; set;}
+        public decimal SalaryByHours {get; set;}
+        public string email {get; set;} = null!;
+        public decimal WorkedHours {get; set;}
     }
 }
