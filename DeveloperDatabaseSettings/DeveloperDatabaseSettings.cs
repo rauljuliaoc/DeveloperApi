c49acafba723a5ef51f895a0eb1aed2b0509bf73
Raul Juliao <raulj@uninorte.edu.co>
2023-03-02 09:16:11 -0500

    Developer database setting for service

---
+++ DeveloperDatabaseSettings/DeveloperDatabaseSettings.cs
@@ -0,0 +1,9 @@
+namespace DeveloperApi.DeveloperDatabaseSettings;
+
+public class DeveloperDatabaseSettings {
+
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string DevsCollectionNameb { get; set; } = null!;
+
+}
